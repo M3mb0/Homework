42ee7da5162a7889eb2e3f2e12b8689128d9380c
M3mb0 <cristian.ungureanu.86@gmail.com>
2022-11-07 18:58:47 +0200

    using filter function instead

--- map_csv_exercies/filter_csv.py
+++ map_csv_exercies/filter_csv.py
@@ -3,16 +3,25 @@
 import csv
 
 
-lst = []
+# lst = []
+
+def scoli_gimnaziale(line):
+    school = line[0].split(";")
+    if "SCOALA GIMNAZIALA" in school[2] and "Comuna" in line[1]:
+        return True
+    return False
+
 
 with open(r"C:\Users\ravva\PycharmProjects\Homework\map_csv_exercies\schools.csv", encoding="utf8") as fin:
     reader = csv.reader(fin.readlines())
-    for row in reader:
-        school = row[0].split(";")
-        if "SCOALA GIMNAZIALA" in school[2] and "Comuna" in row[1]:
-            lst.append(row)
+    lst = list(filter(scoli_gimnaziale, reader))
+    # for row in reader:
+    #     school = row[0].split(";")
+    #     if "SCOALA GIMNAZIALA" in school[2] and "Comuna" in row[1]:
+    #         lst.append(row)
 
 with open(r"C:\Users\ravva\PycharmProjects\Homework\map_csv_exercies\secondary_schools.csv",
           "w", encoding="utf8", newline="") as fout:
     writer = csv.writer(fout)
     writer.writerows(lst)
+# print(lst)
