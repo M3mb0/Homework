6b66a1246a9ff583f3f781f78d4a8056589c867f
M3mb0 <cristian.ungureanu.86@gmail.com>
2022-12-02 14:20:37 +0200

    menu function small changes - removed select_compliant command and add clear screen function + small changes

--- complaint_booklet+sqlite/main_menu.py
+++ complaint_booklet+sqlite/main_menu.py
@@ -1,23 +1,30 @@
+import time
+import os
 
 
 def menu():
-    print("""HELP PANEL
+    print("""\n\tHELP PANEL
 
     Please use the following commands:
     
     add_complaint - use this command for adding a complaint (requires name and the complaint)
-    select_complaint - use this command for searching a complaint by id (requires the id)
-    modify_status - use this command for changing complaint status in solved (requires the id)
+    solve_complaint - use this command for changing complaint status in solved (requires the id)
     select_complaint_by_status  - use this command for searching the solved or unresolved complaints
                                   (requires to enter 1 or 0. 
                                   0 = not solved 1 = solved)
     select_all - use this command for displaying all complaints
-    exit - use this command to exit""")
+    exit - use this command to exit\n""")
 
 
 def intro():
-    print("""Welcome to our COMPLAINT BOOKLET.
+    print("""\n\tWelcome to our COMPLAINT BOOKLET.
     
     For start working please enter your command
     For seeing the commands please type: help
-    To exit please type: exit""")
+    To exit please type: exit\n""")
+
+
+def clear_screen():
+    """Clears the screen when is called with a delay of 1 second and takes no arguments"""
+    time.sleep(1)
+    os.system('cls')
