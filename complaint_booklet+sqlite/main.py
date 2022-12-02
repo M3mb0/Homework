bf9bea39b950000e2cde08cfe58ae92b0bf50536
M3mb0 <cristian.ungureanu.86@gmail.com>
2022-12-02 14:25:48 +0200

    added the date when the complaint was made and some error handling

--- complaint_booklet+sqlite/main.py
+++ complaint_booklet+sqlite/main.py
@@ -1,5 +1,8 @@
+import sqlite3
+from datetime import datetime
+
 from data_base import Database
-from main_menu import menu, intro
+from main_menu import menu, intro, clear_screen
 
 my_db = Database()
 
@@ -7,28 +10,33 @@ while True:
     intro()
     user_input = input("Please enter your command:")
     if user_input == "help":
+        clear_screen()
         menu()
     elif user_input == "add_complaint":
-        name = input("Hello. Please enter your name: ")
-        complaint = input("Please describe your issue: ")
-        my_db.add_complaint(name, complaint)
-    elif user_input == "select_complaint":
         while True:
             try:
-                user_id = int(input("Please enter the id: "))
-                my_db.select_compliant(user_id)
-            except ValueError as err:
+                clear_screen()
+                name = input("Hello. Please enter your name: ")
+                email = input("Please enter your email: ")
+                complaint = input("Please describe your issue: ")
+                date = str(datetime.now().strftime("%Y-%b-%d %H:%M"))
+                my_db.add_complaint(name, email, complaint, date)
+                break
+            except sqlite3.IntegrityError as err:
                 print(err)
-    elif user_input == "modify_status":
+    elif user_input == "solve_complaint":
         while True:
             try:
+                clear_screen()
                 user_id = int(input("Please enter the id: "))
-                my_db.modify_status_solved(user_id)
+                my_db.solve_complaint(user_id)
+                break
             except ValueError as err:
                 print(err)
     elif user_input == "select_complaint_by_status":
         while True:
             try:
+                clear_screen()
                 my_status = int(input("Please enter the status: "))
                 if my_status in [0, 1]:
                     my_db.select_complaint_by_status(my_status)
@@ -38,9 +46,12 @@ while True:
             except ValueError as err:
                 print(err)
     elif user_input == "select_all":
+        clear_screen()
         my_db.select_all_complaints()
     elif user_input == "exit":
+        clear_screen()
         print("Goodbye!")
         break
     else:
+        clear_screen()
         print("Incorrect command. Please use 'help' to see the available commands")
