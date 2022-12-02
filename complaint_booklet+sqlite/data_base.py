95a29e80a4100190861c92684c2b213b298c4c40
M3mb0 <cristian.ungureanu.86@gmail.com>
2022-12-02 14:24:22 +0200

    select_compliant method change, now returns if it gets an ID.solve_complain method name changed and functionality, date and email added to the table.

--- complaint_booklet+sqlite/data_base.py
+++ complaint_booklet+sqlite/data_base.py
@@ -10,29 +10,35 @@ class Database:
             """CREATE TABLE IF NOT EXISTS "complaints"(
                 "id"	INTEGER NOT NULL UNIQUE,
                 "name" TEXT NOT NULL,
+                "email" TEXT NOT NULL UNIQUE,
                 "complaint" TEXT NOT NULL,
-                "date" datetime DEFAULT CURRENT_TIMESTAMP,
-                "status" INTEGER DEFAULT 0
+                "date" TEXT NOT NULL,
+                "status" INTEGER DEFAULT 0,
                 PRIMARY KEY("id" AUTOINCREMENT)
             );
             """)
 
-    def add_complaint(self, name, compliant):
+    def add_complaint(self, name, email, compliant, date):
 
-        self.my_cursor.execute("""INSERT INTO complaints ('name', 'complaint', 'date') 
-        VALUES (?, ?, datetime('now', 'localtime'));""", (name, compliant))
+        self.my_cursor.execute("""INSERT INTO complaints ('name', 'email', 'complaint', 'date') 
+        VALUES (?, ?, ?, ?);""", (name, email, compliant, date))
         self.connection.commit()
 
     def select_compliant(self, user_id):
 
         id_select = self.my_cursor.execute("""SELECT * FROM complaints WHERE id IN(?);""", (user_id,))
-        print(id_select.fetchall())
+        fetch = id_select.fetchall()
+        if fetch:
+            return fetch
 
-    def modify_status_solved(self, user_id):
+    def solve_complaint(self, user_id):
 
-        self.my_cursor.execute("""UPDATE complaints SET status=1 WHERE id IN(?);""", (user_id,))
-        self.connection.commit()
-        print(f"Complaint with id {user_id} is solved")
+        if Database.select_compliant(self, user_id):
+            self.my_cursor.execute("""UPDATE complaints SET status=1 WHERE id IN(?);""", (user_id,))
+            self.connection.commit()
+            print(f"Complaint with id {user_id} is solved")
+        else:
+            print(f"ID {user_id} not found")
 
     def select_complaint_by_status(self, status):
 
